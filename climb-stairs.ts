990ecc9b320a81abb163c89e40bb19a784222e26
mykel68 <olowookeremicheal9568@gmail.com>
2025-03-03 16:24:45 +0100

    feat:climbing stairs

---
+++ climb-stairs.ts
@@ -0,0 +1,13 @@
+function climbStairs(n: number): number {
+    if (n <= 1) return 1;
+
+    let dp: number[] = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
